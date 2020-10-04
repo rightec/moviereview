2c450aea3d0abfbad49251c1c7befe44b4c8c4c7
Fernando Morani <morani.rightec@gmail.com>
2020-10-04 20:50:45 +0200

    add background and data

--- src/App.js
+++ src/App.js
@@ -1,26 +1,58 @@
+//import React from 'react';
+//import logo from './images/logoRightec.png';
+//import { ReactComponent as Logo } from './logo.svg';
+//import './App.css';
+
+
+
+
+
+/*
+function App() { 
+  return (
+    <div class="square">
+      <div class="timeZone">{retData()}</div>
+      <h1>Number of seconds is {this.state.seconds}</h1>
+    </div>  
+  );
+}
+*/
+
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    seconds: 1
+  };
+
+  
+  componentDidMount() {
+    this.interval = setInterval(() => {
+      this.myTime = Date().toLocaleLowerCase()
+    }, 1000);
+
+    this.timer = setInterval(() => {
+      this.setState({ seconds: this.state.seconds + 1 });
+    }, 1000);
+    
+  }
+
+  // eslint-disable-next-line react/no-typos
+  componentWillUnMount() {
+    clearInterval(this.timer);
+  }
+
+  render() {
+    return (
+      <div className="App">
+        {/*<h1>Number of seconds is {this.state.seconds}</h1>*/}
+        <div class="square">
+          <div class="timeZone">{this.myTime}</div>          
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
