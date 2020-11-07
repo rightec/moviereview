00655f78e26bd074ef818d383a24a879f10255c9
Fernando Morani <morani.rightec@gmail.com>
2020-11-07 11:24:50 +0100

    Project Design started

--- src/App.js
+++ src/App.js
@@ -21,38 +21,39 @@ function App() {
 import React from 'react';
 import './App.css';
 
-class App extends React.Component {
-  state = {
-    seconds: 1
-  };
+class App extends
+React.Component {
+    state = {
+        seconds: 1
+    };
 
-  
-  componentDidMount() {
-    this.interval = setInterval(() => {
-      this.myTime = Date().toLocaleLowerCase()
-    }, 1000);
 
-    this.timer = setInterval(() => {
-      this.setState({ seconds: this.state.seconds + 1 });
-    }, 1000);
-    
-  }
+    componentDidMount() {
+        this.interval = setInterval(() => {
+            this.myTime = Date().toLocaleLowerCase()
+        }, 1000);
 
-  // eslint-disable-next-line react/no-typos
-  componentWillUnMount() {
-    clearInterval(this.timer);
-  }
+        this.timer = setInterval(() => {
+            this.setState({ seconds: this.state.seconds + 1 });
+        }, 1000);
 
-  render() {
-    return (
-      <div className="App">
-        {/*<h1>Number of seconds is {this.state.seconds}</h1>*/}
-        <div class="square">
-          <div class="timeZone">{this.myTime}</div>          
-        </div>
-      </div>
-    );
-  }
+    }
+
+    // eslint-disable-next-line react/no-typos
+    componentWillUnMount() {
+        clearInterval(this.timer);
+    }
+
+    render() {
+        return ( <
+            div className = "App" > { /*<h1>Number of seconds is {this.state.seconds}</h1>*/ } <
+            div class = "square" >
+            <
+            div class = "timeZone" > { this.myTime } < /div>           < /
+            div > <
+            /div>
+        );
+    }
 }
 
 export default App;
