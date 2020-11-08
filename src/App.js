cddfa989b8c92b193f75d41cb0b3ea437461c5ae
Fernando Morani <morani.rightec@gmail.com>
2020-11-08 21:18:33 +0100

    Add Header React Component

--- src/App.js
+++ src/App.js
@@ -18,42 +18,50 @@ function App() {
 }
 */
 
-import React from 'react';
-import './App.css';
+import React from 'react'
+import TagList from './components/HeaderComp'
+import './App.css'
 
-class App extends
-React.Component {
-    state = {
-        seconds: 1
-    };
+const tags = ['HOME', 'REVIEW']
 
+class App extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = { seconds: 1 }
+    // props.tags = ['HOME', 'REVIEW']
+    // props.selectedTags = props.storedTags[0]
+  }
 
-    componentDidMount() {
-        this.interval = setInterval(() => {
-            this.myTime = Date().toLocaleLowerCase()
-        }, 1000);
+  componentDidMount () {
+    this.interval = setInterval(() => {
+      this.myTime = Date().toLocaleLowerCase()
+    }, 1000)
 
-        this.timer = setInterval(() => {
-            this.setState({ seconds: this.state.seconds + 1 });
-        }, 1000);
+    this.timer = setInterval(() => {
+      this.setState({ seconds: this.state.seconds + 1 })
+    }, 1000)
+  }
 
-    }
+  // eslint-disable-next-line react/no-typos
+  componentWillUnMount () {
+    clearInterval(this.timer)
+  }
 
-    // eslint-disable-next-line react/no-typos
-    componentWillUnMount() {
-        clearInterval(this.timer);
-    }
-
-    render() {
-        return ( <
-            div className = "App" > { /*<h1>Number of seconds is {this.state.seconds}</h1>*/ } <
-            div class = "square" >
-            <
-            div class = "timeZone" > { this.myTime } < /div>           < /
-            div > <
-            /div>
-        );
-    }
+  render () {
+    return (
+      <div>
+        <div className='App'> {/* <h1>Number of seconds is {this.state.seconds}</h1> */} 
+          <div class='square'>
+            <div class='timeZone'> {this.myTime} </div>
+          </div>
+        </div>
+        <TagList
+          tags={tags}
+          selectedTag={tags[0]}
+        />
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
---
+++ src/components/HeaderComp.jsx
@@ -0,0 +1,31 @@
+// Header component
+// H1 element to show the site Title: Movie Review
+// Unorder list HOME and
+
+import React from 'react'
+import '../App.css'
+
+const TagList = (props) => {
+  return (
+    <div className='SetHeader'>
+      <h1 className='MainHeader'>Movie Review</h1>
+      <div className='ContainTags'>
+        {props.tags.map((tag, index) =>
+          <span className='HeadTags' key={`tag-${index}`}>
+            <a
+              name={tag}
+              onClick={props.onTagClick}
+              className={props.selectedTag === tag ? 'App-link-selected' : 'App-link'}
+              href='www.volturnomorani.it'
+            >
+              {tag}
+            </a>
+            {index === props.tags.length - 1 ? '' : ' | '}
+          </span>
+        )}
+      </div>
+    </div>
+  )
+}
+
+export default TagList
